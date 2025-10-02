0180b1fea05cfa0e0b52c5e03d3151841fefa0fb
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-10-02 08:35:03 -0400

    Added Employee class

--- cc_6.js
+++ cc_6.js
@@ -1,1 +1,13 @@
 //Step 2
+class Employee{
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+
+}
+describe () 
+    return `${this.name} works in ${this.department}`;
+
+
+
