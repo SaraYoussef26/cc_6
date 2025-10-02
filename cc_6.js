8d8c8f05089a96b78ae9abc5ae38b88fa24f0f25
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-10-02 09:12:43 -0400

    Added new employees & managers

--- cc_6.js
+++ cc_6.js
@@ -1,5 +1,5 @@
 //Step 2
-class Employee{
+class Employee {
     constructor(name, department) {
         this.name = name;
         this.department = department;
@@ -8,6 +8,8 @@ class Employee{
 }
 describe () 
     return `${this.name} works in ${this.department}`;
+    
+
 
 //Step 3
 class Manager extends Employee {
@@ -19,3 +21,17 @@ class Manager extends Employee {
         return `${this.name} manages ${this.teamSize} people in ${this.department}`;
     }  
 }
+
+//Step 4
+const emp1 = new Employee ("Ashley Andrews", "HR");
+const emp2 = new Employee ("Martin John", "Finance");
+const mgr1 = new Manager ("John Smith", "Mrketing", "20");
+const mgr2 = new Manager ("Cameron William", "IT", "12");
+
+console.log(emp1.describe());
+console.log(emp2.describe());
+console.log(mgr1.describe());
+console.log(mgr2.describe());
+
+
+
